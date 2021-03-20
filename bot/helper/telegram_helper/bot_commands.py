0008c6582ac18e9e78ac6218b6f36ece322e9ec3
mikiadarsh25 <57398892+mikiadarsh25@users.noreply.github.com>
2021-03-21 01:35:27 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,23 +1,23 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.MirrorCommand = 'mirror'
-        self.UnzipMirrorCommand = 'unzipmirror'
-        self.TarMirrorCommand = 'tarmirror'
-        self.CancelMirror = 'cancel'
-        self.CancelAllCommand = 'cancelall'
+        self.MirrorCommand = 'mirrortgg'
+        self.UnzipMirrorCommand = 'unzipmirrortgg'
+        self.TarMirrorCommand = 'tarmirrortgg'
+        self.CancelMirror = 'canceltgg'
+        self.CancelAllCommand = 'cancelalltgg'
         self.ListCommand = 'list'
-        self.StatusCommand = 'status'
-        self.AuthorizeCommand = 'authorize'
+        self.StatusCommand = 'statustgg'
+        self.AuthorizeCommand = 'authorizetgg'
         self.UnAuthorizeCommand = 'unauthorize'
-        self.PingCommand = 'ping'
+        self.PingCommand = 'pingtgg'
         self.RestartCommand = 'restart'
-        self.StatsCommand = 'stats'
-        self.HelpCommand = 'help'
-        self.LogCommand = 'log'
-        self.CloneCommand = "clone"
-        self.WatchCommand = 'watch'
-        self.TarWatchCommand = 'tarwatch'
+        self.StatsCommand = 'statstgg'
+        self.HelpCommand = 'helptgg'
+        self.LogCommand = 'logtgg'
+        self.CloneCommand = "clonetgg"
+        self.WatchCommand = 'watchtgg'
+        self.TarWatchCommand = 'tarwatchtgg'
         self.deleteCommand = 'del'
 
 BotCommands = _BotCommands()
